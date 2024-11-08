11443c76488bce33073b15e1104c3ba082cb6de1
DfrtJulien <de.freitas.julien78@gmail.com>
2024-11-08 12:22:37 +0100

    mise en place de la home page

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 router.get("", (req, res) => {
-  res.render("index");
+  const locals = {
+    title: "NodeJs blog",
+    description: "Un blog simple crée avec NodeJs et MongoDb.",
+  };
+  res.render("index", locals);
+});
+
+router.get("/about", (req, res) => {
+  res.render("about");
 });
 
 module.exports = router;
