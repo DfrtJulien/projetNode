cf278819efc07f91656d26c2b9acb6d2c842007d
DfrtJulien <de.freitas.julien78@gmail.com>
2024-11-17 16:30:04 +0100

    mise en place de da deconnexion

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -8,6 +8,22 @@ const jwt = require("jsonwebtoken");
 const adminLayout = "../views/layouts/admin";
 const jwtSecret = process.env.JWT_SECRET;
 
+const authMiddleware = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
 router.get("/admin", async (req, res) => {
   try {
     const locals = {
